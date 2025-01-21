c2cfe689109de754b6bcb92e8ded722796176cbf
victorivis <knightdragonred@gmail.com>
2025-01-20 23:06:51 -0300

    refactor: padroniza nomes em ingles e utiliza o cache com local storage. Ver pull request #6

--- js/requests.js
+++ js/requests.js
@@ -1,83 +1,162 @@
-async function lerToken() {
-    const chave = await fetch('../configs/configs.json');
-    const objeto = await chave.json();
+const maxAge = 2 * 60 * 60 * 1000; // 2 horas
+const keyToAge = "date";
+const localStorageKey = "loopis-news";
+
+// Recria o localStorage se ele estiver por um prazo em milissegundos maior que maxAge
+function getLocalStorage(){
+    const currentDate = new Date();
+
+    const currentItem = localStorage.getItem(localStorageKey);
+
+    let currentObject = !currentItem ? {keyToAge: currentDate}: JSON.parse(currentItem);
+
+    if(!currentObject[keyToAge]){
+        currentObject = {};
+        currentObject[keyToAge] = currentDate;
+    }
+    else{
+        const objectDate = new Date(currentObject[keyToAge]);
+
+        if(currentDate.getTime() - objectDate.getTime() > maxAge){
+            currentObject = {};
+            currentObject[keyToAge] = currentDate;
+        }
+    }
+   
+    return currentObject;
+}
+
+function setLocalStorage(atribute, value){
+    const currentItem = localStorage.getItem(localStorageKey);
+
+    let currentObject = getLocalStorage();
     
-    console.log(objeto.token);
-    return objeto.token;
+    currentObject[atribute] = value;
+    localStorage.setItem(localStorageKey, JSON.stringify(currentObject));
 }
 
-function requisicaoEverything(sortBy) {
-    const limiteResultados = 30;
-    const sites = "domains=nytimes.com,cnn.com,washingtonpost.com&";
-    const idioma = 'en';
+async function readToken() {
+    const atributeName = 'apiKey';
+    const allPages = getLocalStorage();
+    let apiKeyToken = allPages[atributeName];
 
+    //Se nao estiver no localStorage busca e salva
+    if(!apiKeyToken){
+        const configsFile = await fetch('../configs/configs.json');
+        const configsFileJSON = await configsFile.json();
+        setLocalStorage(atributeName, configsFileJSON.token);
+        apiKeyToken = configsFileJSON.token;
+    }
+
+    return apiKeyToken;
+}
+
+const maxResultNumber=6;
+const sitesDomain = "domains=nytimes.com,cnn.com,washingtonpost.com";
+const language = 'en';
+
+// Retorna uma funcao que faz uma requisicao para o endereco everything da API
+// Essa funcao ordena com base no atributo sortBy
+// Ela tambem recebe um atributeName que permite encontra-lo no localStorage
+function factoryRequisitionEverything(sortBy, atributeName) {
     return async (pesquisa)=>{
-        const token = await lerToken();
-        const url = `https://newsapi.org/v2/everything?language=${idioma}&sortBy=${sortBy}&apiKey=${token}&${!pesquisa ? sites : `q=${pesquisa}&`}pageSize=${limiteResultados}`;
+        const allPages = getLocalStorage();
+        let currentPage = allPages[atributeName]
 
-        try {
-            const resposta = await fetch(url, {
+        //Se nao estiver no localStorage busca e salva
+        if(!currentPage){
+            const token = await readToken();
+
+            // Precisa realizar uma query de pesquisa ou especificar os dominios de sites
+            // senao a API nao retorna nada, pois diz que eh muito abrangente
+            const url = `https://newsapi.org/v2/everything?language=${language}&sortBy=${sortBy}&apiKey=${token}&${!pesquisa ? sitesDomain  : `q=${pesquisa}`}&pageSize=${maxResultNumber}`;
+
+            const response = await fetch(url, {
                 method: 'GET'
             });
+            currentPage = await response.json();
+            setLocalStorage(atributeName, currentPage);
+            console.log(atributeName, "not in cache");
+        }
+        else{
+            console.log(atributeName, "in cache");
+        }
 
-            if (!resposta.ok) {
-                throw new Error(`Erro ao buscar dados.${resposta.status}`);
-            }
-            const objeto = await resposta.json();
-            
-            return objeto;
-        }
-        catch (erro) {
-            console.error("Erro ao se comunicar com a API:", erro);
-            throw erro;
-        }
+        return currentPage;
     }
 }
 
-function requisicaoHeadlines(categoria) {
-    const limiteResultados = 30;
-    const idioma = 'en';
-
+// Retorna uma funcao que faz uma requisicao para o endereco everything da API
+// Essa chamada eh feita com base no atributo sortBy
+function factoryRequisitionHeadlines(category, atributeName) {
     return async ()=>{
-        const token = await lerToken();
-        const url = `https://newsapi.org/v2/top-headlines?category=${categoria}&apiKey=${token}&language=${idioma}&pageSize=${limiteResultados}`;
+        const allPages = getLocalStorage();
+        let currentPage = allPages[atributeName]
 
-        try {
-            const resposta = await fetch(url, {
+        //Se nao estiver no localStorage busca e salva
+        if(!currentPage){
+            const token = await readToken();
+            const url = `https://newsapi.org/v2/top-headlines?category=${category}&apiKey=${token}&language=${language}&pageSize=${maxResultNumber}`;
+
+            const response = await fetch(url, {
                 method: 'GET'
             });
+            currentPage = await response.json();
+            setLocalStorage(atributeName, currentPage);
+            console.log(atributeName, "not in cache");
+        }
+        else{
+            console.log(atributeName, "in cache");
+        }
 
-            if (!resposta.ok) {
-                throw new Error(`Erro ao buscar dados. ${resposta.status}`);
-            }
-            const objeto = await resposta.json();
-            return objeto;
-        }
-        catch (erro) {
-            console.error("Erro ao se comunicar com a API:", erro);
-            throw erro;
-        }
+        return currentPage;
     }
 }
 
-//Pesquisas feitas sem categorias
-const fetchDestaques = requisicaoEverything('popularity');
-const fetchUltimasNoticias = requisicaoEverything('publishedAt');
-const fetchPesquisa = requisicaoEverything('publishedAt');
+// Funcoes para requisicoes de pesquisas sem categorias
+async function fetchHighlights(){
+    const results = factoryRequisitionEverything('popularity', 'highlights');
+    return await results();
+}
 
-//Recebe as notícias mais recentes dessas categorias
-const fetchEsporte = requisicaoHeadlines('sports');
-const fetchPolitica = requisicaoHeadlines('politics');
-const fetchTecnologia = requisicaoHeadlines('technology');
+async function fetchLatest() {
+    const results = factoryRequisitionEverything('publishedAt', 'latest');
+    return await results();
+}
 
-async function fetchCategorias(){
-    const esporte = await fetchEsporte();
-    const politica = await fetchPolitica();
-    const tecnologia = await fetchTecnologia();
+async function fetchSearch() {
+    const results = factoryRequisitionEverything('relevancy', 'search');
+    return await results();
+}
+
+
+// Funcoes para notícias mais recentes das categorias abaixo
+async function fetchSports() {
+    const results = factoryRequisitionHeadlines('sports', 'sports');
+    return await results();
+}
+
+async function fetchPolitics() {
+    const results = factoryRequisitionHeadlines('politics', 'politics');
+    return await results();
+}
+
+async function fetchTechnology() {
+    const results = factoryRequisitionHeadlines('technology', 'technology');
+    return await results();
+}
+
+
+// Retorna um objeto contendo as 3 categorias utilizadas no site
+// returns { sports: [{...}], politics: [{...}], technology: [{...}] }
+async function fetchCategories(){
+    const sports = await fetchSports();
+    const politics = await fetchTechnology();
+    const technology = await fetchSports();
 
     return {
-        esporte,
-        politica,
-        tecnologia
+        sports,
+        politics,
+        technology
     };
 }
