64c93e83ea1befc90eb37154260a1cf6cec1553e
tasso.moreira <tassomoreira1@gmail.com>
2025-01-23 18:13:50 -0300

    feat: implementa renderizacao da noticia principal na pagina de noticia

--- js/noticia.js
+++ js/noticia.js
@@ -1,6 +1,25 @@
 import { fetchCategories, fetchLatest, saveCurrentArticle, getCurrentArticle } from "./requests.js";
 
-function createNewsItem(news, newsList) {
+function formatDate(dateString) {
+    const date = new Date(dateString);
+
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+
+    return `${day}/${month}/${year} ${hours}h${minutes}`;
+}
+
+function validateNewsData(newsData) {
+    if(newsData.content !== null) {
+        saveCurrentArticle(newsData);
+        window.location.href = "../html/noticia.html";
+    }
+}
+
+function createSidebarNewsItem(news, newsList) {
     const newsItem = document.createElement('li');
     newsItem.classList.add('news-item');
 
@@ -21,36 +40,72 @@ function createNewsItem(news, newsList) {
     newsItem.appendChild(newsInfo);
 
     newsItem.addEventListener("click", () => {
-        saveCurrentArticle(news);
+        validateNewsData(news);
     });
 
     newsList.appendChild(newsItem);
 }
 
-async function renderNews(newsType, containerId) {
-    const newsList = document.querySelector(`#${containerId}`);
+function renderMainNews() {
+    try {
+        const newsImage = document.querySelector("#main-news-image");
+        const newsTitle = document.querySelector("#main-news-title");
+        const newsDescription = document.querySelector("#main-news-description");
+        const newsAutor = document.querySelector("#main-news-autor");
+        const newsSource = document.querySelector("#main-news-source");
+        const newsDate = document.querySelector("#main-news-date");
+        const newsContent = document.querySelector("#main-news-content");
 
-    let newsData;
-    switch (newsType) {
-        case "latest":
-            newsData = await fetchLatest(); break;
-        case "technology":
-            newsData = (await fetchCategories()).technology; break;
-        case "sports":
-            newsData = (await fetchCategories()).sports; break;
-        case "politics" :
-            newsData = (await fetchCategories()).politics; break;
-        default:
-            console.error("Tipo de notícia desconhecido");
-            return;
-    }
+        const currentNews = getCurrentArticle();
 
-    for (let news of newsData.articles) {
-        createNewsItem(news, newsList);
+        newsImage.src = currentNews.urlToImage || "../assets/images/image-not-available.png";
+
+        newsTitle.textContent = currentNews.title;
+
+        newsDescription.textContent = currentNews.description.replace(/\n/g, '<br>');
+
+        newsAutor.textContent = currentNews.author;
+
+        newsSource.textContent = currentNews.source.name;
+        newsSource.href = currentNews.url;
+
+        newsDate.textContent = formatDate(currentNews.publishedAt);
+
+        newsContent.textContent = currentNews.content;
+    } catch(e) {
+        console.error("Erro ao tentar renderizar notícia principal: " + e);
     }
 }
 
-renderNews("latest", "latest-news");
-renderNews("technology", "technology-news");
-renderNews("sports", "sports-news");
-renderNews("politics", "politics-news")
+async function renderSidebarNews(newsType, containerId) {
+    try {
+        const newsList = document.querySelector(`#${containerId}`);
+
+        let newsData;
+        switch (newsType) {
+            case "latest":
+                newsData = await fetchLatest(); break;
+            case "technology":
+                newsData = (await fetchCategories()).technology; break;
+            case "sports":
+                newsData = (await fetchCategories()).sports; break;
+            case "politics" :
+                newsData = (await fetchCategories()).politics; break;
+            default:
+                console.error("Tipo de notícia desconhecido");
+                return;
+        }
+
+        for (let news of newsData.articles) {
+            createSidebarNewsItem(news, newsList);
+        }
+    } catch(e) {
+        console.error("Erro ao tentar renderizar notícias da sidebar: " + e);
+    }
+}
+
+renderMainNews();
+renderSidebarNews("latest", "latest-news");
+renderSidebarNews("technology", "technology-news");
+renderSidebarNews("sports", "sports-news");
+renderSidebarNews("politics", "politics-news");
